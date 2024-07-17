a8614da716ced3e504dadbb4323144bd09d7c850
geisabitt <ageisislane@gmail.com>
2024-07-17 12:38:59 -0300

    refatorando para a vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Testes Api Intagram",
-  description: "",
+  description: "Descrição",
 };
 
 export default function RootLayout({
@@ -15,7 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <h2>Adicionando um titulo na raiz</h2>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import InstagramAuth from "./form-insta";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>testando a pagina de exibição</h1>
       <InstagramAuth />
     </main>
   );
