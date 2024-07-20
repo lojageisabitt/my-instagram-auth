9d3e01344f23102f50ae799760f50138a2d4f96e
geisabitt <ageisislane@gmail.com>
2024-07-20 14:48:20 -0300

    endpoind adicionado para resolver cors

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -8,13 +8,14 @@ export async function POST(req: NextRequest) {
 
     const CLIENT_ID = process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID;
     const CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
+    const AUTHORIZATION_CODE = process.env.INSTAGRAM_AUTHORIZATION_CODE;
     const REDIRECT_URI = process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI;
     const response = await axios.post(
-      `https://api.instagram.com/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&code=${code}`
+      `https://api.instagram.com/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=${AUTHORIZATION_CODE}&redirect_uri=${REDIRECT_URI}&code=${code}`
     );
     console.log('Response >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', response)
 
-    return NextResponse.json({ status: 200, access_token: response.data.access_token });
+    return NextResponse.json({ status: 200, response });
   } catch (error: any) {
     console.error('Erro ao obter o token de acesso:', error);
     return NextResponse.json({ status: 500, error: 'Erro ao obter o token de acesso' });
--- src/app/form-insta2.tsx
+++ src/app/form-insta2.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 
 export default function InstagramAuth2() {
   const [userData, setUserData] = useState(null);
+  const [getAccessToken, setGetAccessToken] = useState(null);
   const CLIENT_ID = process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI;
   const AUTH_URL = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
@@ -12,6 +13,8 @@ export default function InstagramAuth2() {
   const fetchAccessToken = async (code: string) => {
     try {
       const response = await axios.post("/api/post", { code });
+      console.log(response.data);
+      setGetAccessToken(response.data.access_token);
       return response.data.access_token;
     } catch (error) {
       console.error("Erro ao obter o token de acesso:", error);
@@ -19,10 +22,13 @@ export default function InstagramAuth2() {
     }
   };
 
-  const fetchUserData = async (accessToken: string) => {
+  const fetchUserData = async () => {
     try {
       const response = await axios.get("/api/get", {
-        params: { accessToken },
+        params: {
+          fields: "id,username,account_type,media_count,profile_picture_url",
+          access_token: "IGQWRPYV9URGNhVUlPMnlWWnJYZAGZANR0UtMzFFN1hLR3U0VE1hYWMtN1FJNm9Mc0tVVW1yTnhLd2w2R2RjYUNmNUVnZAVJBelpIVko3dm11QlZAwYzhQN18ya25nRE1NcnFTbk0wTUZAWU2VrVjZAlZAW5hbDREdmVhNGsZD",
+        },
       });
       setUserData(response.data);
       console.log(response.data);
@@ -34,25 +40,28 @@ export default function InstagramAuth2() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
+    fetchUserData();
 
-    if (code) {
-      fetchAccessToken(code).then((accessToken) => {
-        if (accessToken) {
-          fetchUserData(accessToken);
-        }
-      });
-    }
+    // if (code) {
+    //   fetchAccessToken(code).then((accessToken) => {
+    //     if (accessToken) {
+    //       fetchUserData(accessToken);
+    //     }
+    //   });
+    // }
   }, []);
 
   return (
     <div>
       <h1>Instagram Auth</h1>
+      {getAccessToken ? <p>`${getAccessToken}`</p> : <p>reservado pata token de acesso</p>}
       {!userData ? (
         <a href={AUTH_URL}>Login with Instagram</a>
       ) : (
         <div>
           <h2>Welcome</h2>
           <p>{JSON.stringify(userData)}</p>
+
           <img src={"userData!.profile_picture_url"} alt="Profile Picture" />
         </div>
       )}
