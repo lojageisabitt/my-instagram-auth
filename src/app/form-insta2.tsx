1b818efde4e85a7518ab1a86fd3adb1a0171bd7f
geisabitt <ageisislane@gmail.com>
2024-07-20 15:00:27 -0300

    TESTES 1289

--- src/app/form-insta2.tsx
+++ src/app/form-insta2.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 
 export default function InstagramAuth2() {
   const [userData, setUserData] = useState(null);
-  const [getAccessToken, setGetAccessToken] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
   const CLIENT_ID = process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI;
   const AUTH_URL = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
@@ -14,7 +14,7 @@ export default function InstagramAuth2() {
     try {
       const response = await axios.post("/api/post", { code });
       console.log(response.data);
-      setGetAccessToken(response.data.access_token);
+      setAccessToken(response.data.access_token);
       return response.data.access_token;
     } catch (error) {
       console.error("Erro ao obter o token de acesso:", error);
@@ -22,13 +22,10 @@ export default function InstagramAuth2() {
     }
   };
 
-  const fetchUserData = async () => {
+  const fetchUserData = async (accessToken: string) => {
     try {
       const response = await axios.get("/api/get", {
-        params: {
-          fields: "id,username,account_type,media_count,profile_picture_url",
-          access_token: "IGQWRPYV9URGNhVUlPMnlWWnJYZAGZANR0UtMzFFN1hLR3U0VE1hYWMtN1FJNm9Mc0tVVW1yTnhLd2w2R2RjYUNmNUVnZAVJBelpIVko3dm11QlZAwYzhQN18ya25nRE1NcnFTbk0wTUZAWU2VrVjZAlZAW5hbDREdmVhNGsZD",
-        },
+        params: { accessToken },
       });
       setUserData(response.data);
       console.log(response.data);
@@ -40,29 +37,34 @@ export default function InstagramAuth2() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
-    fetchUserData();
 
-    // if (code) {
-    //   fetchAccessToken(code).then((accessToken) => {
-    //     if (accessToken) {
-    //       fetchUserData(accessToken);
-    //     }
-    //   });
-    // }
+    if (code) {
+      fetchAccessToken(code).then((accessToken) => {
+        if (accessToken) {
+          fetchUserData(accessToken);
+        }
+      });
+    }
   }, []);
 
   return (
     <div>
       <h1>Instagram Auth</h1>
-      {getAccessToken ? <p>`${getAccessToken}`</p> : <p>reservado pata token de acesso</p>}
       {!userData ? (
         <a href={AUTH_URL}>Login with Instagram</a>
       ) : (
         <div>
           <h2>Welcome</h2>
           <p>{JSON.stringify(userData)}</p>
-
           <img src={"userData!.profile_picture_url"} alt="Profile Picture" />
+        </div>
+      )}
+      <h2>Access Token</h2>
+      {!accessToken ? (
+        <p className="">Espaço reservado para o token de acesso</p>
+      ) : (
+        <div>
+          <h2>{accessToken}</h2>
         </div>
       )}
     </div>
